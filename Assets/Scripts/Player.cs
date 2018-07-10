0417da0e512eda5caa52e47276781b394a5621cd
Unknown <Shakugaan01@gmail.com>
2018-07-10 17:53:26 +0200

    First map
    rawr

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private float MS = 15.0f;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * MS;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+
+        }
+    }
+    private void Rotate(float angle)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public float distance = 3.0f;
+    public float height = 3.0f;
+    public Transform Target;
+    
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Target.transform.position + new Vector3(0.0f,height,-distance);
+	}
+}
